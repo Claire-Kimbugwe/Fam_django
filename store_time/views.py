92fd9219cd0fa11a4ee1a5e28eb19c9b9aeccb79
claire muwanguzi <kclaire56@yahoo.com>
2019-03-26 13:53:25 -0700

    added the  display of dynamic information

--- store_time/views.py
+++ store_time/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from store_time.models import Family
 
 # Create your views here.
 def index(request):
     store = 'Dembe'
     num = 9
-    return render(request,'index.html',{"store":store ,'num':num})
+    families = Family.objects.all().order_by('name')
+    # how to filter
+    kim = Family.objects.filter(name__contains='Kim')
+    return render(request,'index.html', {"store":store ,'num':num,
+                                         'families':families,'kim':kim})
+
 def results(request):
     stations = request.args.get('num')
     time = stations * 30
